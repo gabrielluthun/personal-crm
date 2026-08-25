ea665ae75b8b84aef52d07770382a2928600c7ed
Gabriel Luthun <gabriel.luthun@gmail.com>
2026-08-25 16:45:45 +0200

    feat(data): re-export mock fixtures barrel

---
+++ lib/data/mocks/index.ts
@@ -0,0 +1,7 @@
+export {
+  MOCK_ENTREPRISES,
+  MOCK_ENTREPRISE_IDS,
+} from "@/lib/data/mocks/entreprises.mock";
+export { MOCK_CONTACTS } from "@/lib/data/mocks/contacts.mock";
+export { MOCK_JOB_OFFERS } from "@/lib/data/mocks/job-offers.mock";
+export { MOCK_TEMPLATES } from "@/lib/data/mocks/templates.mock";
